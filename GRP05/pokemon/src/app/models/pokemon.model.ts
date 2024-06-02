f0362dc6dd0433ddda4b496aae3cc235b19077b5
gabrielLapaOriginal <gabriel.lapa2001@yahoo.com.br>
2024-06-01 23:23:04 -0300

    Gabriel Lapa, Augusto Cesar
    pokeapp completo.

---
+++ GRP05/pokemon/src/app/models/pokemon.model.ts
@@ -0,0 +1,7 @@
+export interface Pokemon {
+  id: number;
+  name: string;
+  type: string;
+  abilities: string[];
+  front_default: string;
+}
---
+++ GRP05/pokemon/src/app/services/via-cep.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ViaCEPService {
+
+  constructor(private httpClient: HttpClient ) {}
+  getViaCepService(cep: string = '52011210') {
+    return this.httpClient.get(`http://viacep.com.br/ws/${cep}/json/`);
+  }
+}
