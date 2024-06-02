a40060df93aa87bcef34fa3e3406f77adeb2f24c
gabrielLapaOriginal <gabriel.lapa2001@yahoo.com.br>
2024-06-02 09:10:15 -0300

    Correção de um pequeno erro

--- GRP05/pokemon/src/app/services/poke-api.service.ts
+++ GRP05/pokemon/src/app/services/poke-api.service.ts
@@ -5,9 +5,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class PokeAPIService {
-
   constructor(private httpClient: HttpClient) {}
-
   getPokeAPIService(id: number = Math.floor(Math.random() * 100)) {
     return this.httpClient.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
   }
